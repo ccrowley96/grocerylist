d019d73d4560391803174a3807b5de0ebfa1cfb8
Cory <ccrowley96@gmail.com>
2020-05-19 21:59:56 -0700

    heroku router wip

--- server/index.js
+++ server/index.js
@@ -21,14 +21,14 @@ app.use('/api', API);
 // Static Files
 app.use(express.static(path.join(__dirname, '../client/build')));
 
-//Default catch all -> to index.html
-app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+// //Default catch all -> to index.html
+// app.get('*', function(req, res) {
+//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
 
 // //Default catch all -> to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'), function(err) {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'), function(err) {
     if (err) {
       res.status(500).send(err)
     }
